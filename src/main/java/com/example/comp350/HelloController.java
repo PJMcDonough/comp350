8870549cace80fb268d5b1bd8f168d6f62780f70
jeynerarana <jeyner.arana@gmail.com>
2021-09-21 17:54:52 -0700

    Gui-Side

--- src/main/java/com/example/comp350/HelloController.java
+++ src/main/java/com/example/comp350/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.comp350;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -10,5 +11,9 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    public Button aboutUsBtn;
+    public void aboutUsAction(){
+        System.out.println("Patrick, David, Israel, Jeyner");
     }
 }
