3b8b2d8d111ec46619058ce486c212281b679a2e
Israel Alonso <israel.alonso522@myci.csuci.edu>
2021-09-22 13:25:01 -0700

    Get button working (partially with Patrick)

--- src/main/java/com/example/comp350/HelloController.java
+++ src/main/java/com/example/comp350/HelloController.java
@@ -1,11 +1,24 @@
 package com.example.comp350;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    public Button makeReservation;
+    public void onReservationClick()
+    {
+        System.out.println("Debug Israel Patrick");
+    }
+/*
+    @FXML
+    protected void setMakeReservation()
+    {
+
+    }*/
 
     @FXML
     protected void onHelloButtonClick() {
