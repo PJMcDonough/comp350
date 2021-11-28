48b7b8e3dfcf4a7f648f567893746492025791d9
jeynerarana <jeyner.arana@gmail.com>
2021-11-28 12:11:51 -0800

    David and Patrick's changes

--- src/main/java/com/example/comp350/HelloApplication.java
+++ src/main/java/com/example/comp350/HelloApplication.java
@@ -3,9 +3,11 @@ package com.example.comp350;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
+import javafx.css.Stylesheet;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
